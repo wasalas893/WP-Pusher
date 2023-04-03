63644a046d4e9f578456e720c99a6a7bb419e221
sunanda wasala <48275082+wasalas893@users.noreply.github.com>
2023-04-03 20:42:37 +0530

    Update cool-plugin.php

--- cool-plugin.php
+++ cool-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: Cool Plugin
  * Plugin URI: https://github.com/wppusher/Cool-Plugin
  * Description: OMG this plugin is so cool.
- * Version: 8.89
+ * Version: 8.80
  * Author: WP Pusher
  * Author URI: http://wppusher.com
  * License: GNU GENERAL PUBLIC LICENSE
